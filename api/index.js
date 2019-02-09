168102641a96e6af1781b54897084a4fc789ea2f
chiefoleka <valentinenwakibeya@gmail.com>
2019-02-09 12:55:01 +0100

    add home url

--- api/index.js
+++ api/index.js
@@ -5,11 +5,6 @@ const app = express();
 
 const PORT = 8080;
 
-app.get('*', (req, res) => {
-  res.status(200).send("Hello World")
-});
-
-
 app.get('/home', (req, res) => {
   res.status(200).send("Home url")
 });
